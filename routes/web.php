212ddfa0162ed4def01bcc24b23c7c72e298de06
manamiw <laki.manami.w@gmail.com>
2026-09-16 09:36:31 +0900

    feat:認証機能の実装(Fortify)

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\TagController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// 公開ページ
+Route::get('/', [ContactController::class, 'index']);
+Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
+Route::get('/thanks', [ContactController::class, 'thanks']);
 
-Route::get('/', function () {
-    return view('welcome');
+// 管理画面（認証必須）
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', [AdminController::class, 'index']);
+    Route::get('/admin/contacts/{contact}', [AdminController::class, 'show']);
+
+    Route::get('/admin/tags/{tag}/edit', [TagController::class, 'edit']);
+    Route::put('/admin/tags/{tag}', [TagController::class, 'update']);
 });
