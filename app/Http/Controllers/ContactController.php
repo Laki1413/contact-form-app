a1eebf25b95bc2983ee043b14fb70efb609e5c0a
manamiw <laki.manami.w@gmail.com>
2026-09-16 13:06:49 +0900

    Feat:お問い合わせフォームCRUD実装

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreContactRequest;
+use App\Models\Category;
+use App\Models\Contact;
+use App\Models\Tag;
+
+class ContactController extends Controller
+{
+    //お問い合わせフォーム入力ページ
+    public function index()
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+
+        return view('contact.index', compact('categories', 'tags'));
+    }
+
+    //お問い合わせフォーム確認ページ
+    public function confirm(StoreContactRequest $request)
+    {
+        $validated = $request->validated();
+
+        $category = Category::find($validated['category_id']);
+
+        $tags = Tag::whereIn('id', $validated['tag_ids'] ?? [])->get();
+
+        return view('contact.confirm', compact(
+            'validated',
+            'category',
+            'tags'
+        ));
+    }
+
+    //お問い合わせを送信
+    public function store(StoreContactRequest $request)
+    {
+        $validated = $request->validated();
+
+        $tagIds = $validated['tag_ids'] ?? [];
+        unset($validated['tag_ids']);
+
+        $contact = Contact::create($validated);
+
+        $contact->tags()->sync($tagIds);
+
+        return redirect('/thanks');
+    }
+
+
+    public function thanks()
+    {
+        return view('contact.thanks');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\TagController;
 // 公開ページ
 Route::get('/', [ContactController::class, 'index']);
 Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
+Route::post('/contacts', [ContactController::class, 'store']);
 Route::get('/thanks', [ContactController::class, 'thanks']);
 
 // 管理画面（認証必須）
