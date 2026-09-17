306e579dd7695219027faf3113b6395eb3e66c68
manamiw <laki.manami.w@gmail.com>
2026-09-17 18:26:27 +0900

    Feat:タグ管理機能の実装

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tag;
+use App\Http\Requests\StoreTagRequest;
+use App\Http\Requests\UpdateTagRequest;
+
+class TagController extends Controller
+{
+    //タグを新規作成
+    public function store(StoreTagRequest $request)
+    {
+        Tag::create($request->validated());
+
+        return redirect('/admin');
+    }
+
+    //タグ編集画面を表示
+    public function edit(Tag $tag)
+    {
+        return view('admin.tags.edit', compact('tag'));
+    }
+
+    //タグを更新
+    public function update(UpdateTagRequest $request, Tag $tag)
+    {
+        $tag->update($request->validated());
+
+        return redirect('/admin');
+    }
+
+    //タグを削除
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+
+        return redirect('/admin');
+    }
+}
